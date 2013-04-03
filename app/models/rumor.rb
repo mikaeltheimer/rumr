0764ad208aec1cd64feb36ddb71aa094b1d7164f
mikael theimer <mikaeltheim@gmail.com>
2013-04-03 19:07:22 -0400

    minor changes

--- app/models/rumor.rb
+++ app/models/rumor.rb
@@ -3,8 +3,6 @@ class Rumor < ActiveRecord::Base
 
   # belongs_to :bubble
 
-  # accepts_nested_attributes_for :bubble
-
   validates :title, presence: true, length: { maximum: 30 }
   validates :content, presence: true, length: { maximum: 300 }
   validates :bubble_id, presence: true
